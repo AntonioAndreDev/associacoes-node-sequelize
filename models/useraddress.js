9937dc0e9bb8f205bb12443dd60fa9a94a70391b
Antonio André <antonioandre1008@gmail.com>
2024-06-25 15:42:21 -0500

    fix: corrige importações

--- models/useraddress.js
+++ models/useraddress.js
@@ -1,24 +1,25 @@
 'use strict';
 const {
-  Model
+    Model, DataTypes
 } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class UserAddress extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+module.exports = (sequelize) => {
+    class UserAddress extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  UserAddress.init({
-    userId: DataTypes.INTEGER,
-    addressId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'UserAddress',
-  });
-  return UserAddress;
+
+    UserAddress.init({
+        userId: DataTypes.INTEGER,
+        addressId: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'UserAddress',
+    });
+    return UserAddress;
 };
